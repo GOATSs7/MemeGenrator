fd05660f12d456edc5bd6c1af969e1fd1a03d6e3
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-12-03 10:42:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+
+let img = document.getElementById("img");
+let button = document.getElementById("button");
+
+async function getMeme() {
+  try {
+    const meme = await fetch("https://meme-api.com/gimme/wholesomememes");
+    const res = await meme.json();
+    return res;
+  } catch (error) {
+    console.error("Error in fetch api" + error);
+    return null;
+  }
+}
+
+async function displayMEME() {
+  try {
+    const MEME = await getMeme();
+    if (MEME) {
+      author.innerHTML = "Author :" + MEME.author;
+      title.innerHTML = "Title :" + MEME.title;
+      img.src = MEME.url;
+      //   img.innerHTML = MEME.url;
+    }
+  } catch (error) {
+    console.error("Error displaying meme: " + error);
+  }
+}
+
+button.onclick = () => {
+  displayMEME();
+};
+
+displayMEME();
